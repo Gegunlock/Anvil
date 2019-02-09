ac5e873260831fcc04377d269bae2af7e6915a31
Graham Gunlock <gegunlock@gmail.com>
2019-02-08 21:13:48 -0600

    Updated VMT class

--- FirstInternal/Source Files/VMT/Vmt.h
+++ FirstInternal/Source Files/VMT/Vmt.h
@@ -9,57 +9,30 @@ public:
 	CVmt(void* pObjectVmtPtr) : m_iVmtSize(CountFunctions())
 	{
 		m_Vmt = *((NTypedefs::t_pVmt)pObjectVmtPtr);
+		m_pOriginalFunctionPtrArray = new void*[m_iVmtSize];
+		m_pCurrentFunctionPtrArray = new void*[m_iVmtSize];
 	}
 
-	virtual int CountFunctions() const;
-	virtual int FindFunctionIndex(void* pfnVoidFunctionPointer) const;
-	virtual void* FindFunctionPtr(int iIndex) const;
+	~CVmt() 
+	{
+		UnhookAll();
+		delete[] m_pOriginalFunctionPtrArray;
+		delete[] m_pCurrentFunctionPtrArray;
+	}
+
+	int CountFunctions() const;
+	int FindFunctionIndex(void* pfnVoidFunctionPointer) const;
+	void* FindFunctionPtr(int iIndex) const;
+
+	bool Unhook(int iIndex);
+	bool UnhookAll();
+	void Hook(void* fpn, int iIndex);
 
 protected:
 	void** m_Vmt;
-	int m_iVmtSize;
+	const int m_iVmtSize;
+
+	void* m_pOriginalFunctionPtrArray;
+	void* m_pCurrentFunctionPtrArray;
 };
 
-class CVmtHookManager : public CVmt
-{
-public:
-	~CVmtHookManager() 
-	{
-
-	}
-
-	bool UnhookAll() 
-	{
-		for (int i = 0; i < m_iVmtSize; i++) 
-		{
-			if (!Unhook(i))
-				return false;
-		}
-		return true;
-	}
-
-	bool Unhook(int iIndex) 
-	{
-		if (m_pfnOriginalFunctionPointer[iIndex] = NULL)
-			return false;
-		else 
-		{
-			m_Vmt[iIndex] = m_pfnOriginalFunctionPointer[iIndex];
-			m_pfnCurrentFunctionPointer[iIndex] = m_pfnOriginalFunctionPointer[iIndex];
-		}
-	}
-
-	bool Hook(void* fpn, int iIndex) 
-	{
-
-	}
-
-	void UpdateSize(int iNewSize) 
-	{
-		m_iVmtSize = iNewSize;
-	}
-
-private:
-	void* m_pfnOriginalFunctionPointer[5] = {0,0,0,0,0};
-	void* m_pfnCurrentFunctionPointer[1] = {};
-};
